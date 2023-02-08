d1b39dbd681ec45276a3d87fa6222558fa399bff
saratobonOC <sara.tobon@offcorss.com>
2023-02-07 21:17:08 -0500

    -m etiqueta en los puntos de referencia

--- maps/public/javascripts/map.js
+++ maps/public/javascripts/map.js
@@ -9,8 +9,8 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
 }).addTo(map)
 
-var marker = L.marker([6.235143, -75.589028]).addTo(map);
-var marker = L.marker([6.256702, -75.590551]).addTo(map);
+var marker = L.marker([6.235143, -75.589028]).addTo(map).bindPopup('Unidad Deportiva').openPopup();
+var marker = L.marker([6.256702, -75.590551]).addTo(map).bindPopup('Estadio').openPopup();
 
 
 $.ajax({
@@ -19,7 +19,7 @@ $.ajax({
     success: function(result){
         console.log(result);
         result.bicicletas.forEach(function(bici) {
-            L.marker(bici.ubicacion, { title: bici.id}).addTo(map);
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
         });
     }
 })
