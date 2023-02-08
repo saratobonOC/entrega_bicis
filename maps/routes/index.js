2cf2e41f4613e1fc38d5c8df62fb29cbe65270f2
saratobonOC <sara.tobon@offcorss.com>
2023-02-07 21:29:31 -0500

    -m finalizado

--- maps/routes/index.js
+++ maps/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Proyecto Bicicletas' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
